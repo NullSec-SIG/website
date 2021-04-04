f025996f6251d73a6c1f07251eb584489e3b0bcc
benwoo1110 <30431861+benwoo1110@users.noreply.github.com>
2021-04-04 23:09:24 +0800

    Add social links.

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -9,12 +9,20 @@ class Footer extends Component {
       <footer className="footer bg-secondary pl-3 pr-3">
         <span className="ml-lg-5">NULLSEC 2021</span>
         <div className="float-right mr-lg-5">
-          <button className="btn">
+          <a
+            className="btn"
+            href="https://github.com/NullSec-SIG"
+            target="_blank"
+          >
             <FontAwesomeIcon icon={faGithub} size="lg" />
-          </button>
-          <button className="btn">
+          </a>
+          <a
+            className="btn"
+            href="https://www.instagram.com/nullsec.sig/"
+            target="_blank"
+          >
             <FontAwesomeIcon icon={faInstagram} size="lg" />
-          </button>
+          </a>
         </div>
       </footer>
     );
